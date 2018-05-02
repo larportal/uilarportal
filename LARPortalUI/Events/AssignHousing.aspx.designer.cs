7bbdf7eeba6a5ebcc42f830aa09b73c508a8e56b
jbradshaw <jbradshaw@332d60cc-61b5-4faf-922e-d82a48ef0ac1>
2018-05-02 23:28:33 +0000

    Change sort order of events.

--- LARPortalUI/Events/AssignHousing.aspx.designer.cs
+++ LARPortalUI/Events/AssignHousing.aspx.designer.cs
@@ -94,6 +94,15 @@ namespace LarpPortal.Events {
         protected global::System.Web.UI.WebControls.Button btnClose;
         
         /// <summary>
+        /// hidScollPosition control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hidScollPosition;
+        
+        /// <summary>
         /// Master property.
         /// </summary>
         /// <remarks>
