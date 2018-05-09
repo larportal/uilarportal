d71e49b65c964626128dec12dc3c35cfd5d3acae
jbradshaw <jbradshaw@332d60cc-61b5-4faf-922e-d82a48ef0ac1>
2018-05-09 21:23:35 +0000

    Fix sort order for Roles, add filter for events to only show open events.

--- LARPortalUI/Events/RegistrationApproval.aspx.cs
+++ LARPortalUI/Events/RegistrationApproval.aspx.cs
@@ -38,7 +38,7 @@ namespace LarpPortal.Events
                 sParams.Add("@CampaignID", Master.CampaignID);
                 DataTable dtEvents = Classes.cUtilities.LoadDataTable("uspGetCampaignEvents", sParams, "LARPortal", Master.UserName, "RegistrationApproval.Page_PreRender");
 
-                DataView dvEvents = new DataView(dtEvents, "", "StartDate desc", DataViewRowState.CurrentRows);
+                DataView dvEvents = new DataView(dtEvents, "EventStatus like 'Sche%'", "StartDate", DataViewRowState.CurrentRows);
 
                 if (dtEvents.Columns["DisplayValue"] == null)
                     dtEvents.Columns.Add("DisplayValue", typeof(string));
