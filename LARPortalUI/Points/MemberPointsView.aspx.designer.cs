b4ba4dbef2b09459052a1ecd6176074a156660b4
larportal <71445037+larportal@users.noreply.github.com>
2021-09-12 15:55:43 -0400

    MemberPointsView designer

--- LARPortalUI/Points/MemberPointsView.aspx.designer.cs
+++ LARPortalUI/Points/MemberPointsView.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace LarpPortal.Points
         protected global::System.Web.UI.WebControls.DropDownList ddlPointType;
 
         /// <summary>
+        /// chkApplyTo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkApplyTo;
+
+        /// <summary>
         /// mvPointList control.
         /// </summary>
         /// <remarks>
