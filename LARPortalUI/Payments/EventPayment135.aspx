83cf872469c06f98ddcacad7c9d436e5e6a456ed
larportal <71445037+larportal@users.noreply.github.com>
2024-01-18 20:42:55 -0500

    Payment pages

---
+++ LARPortalUI/Payments/EventPayment135.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EventPayment135.aspx.cs" Inherits="LarpPortal.Payments.EventPayment135" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            }); var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5MQHDGS');</script>
+    <!-- End Google Tag Manager -->
+
+    <title></title>
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MQHDGS"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <div class="contentArea">
+        <aside></aside>
+        <div class="mainContent tab-content col-lg-6 input-group">
+            <section id="campaign-info" class="campaign-info tab-pane active">
+                <form id="frmHidFields" runat="server">
+                    <div role="form" class="form-horizontal">
+                        <div class="col-sm-12 NoPadding">
+                            <h1 class="col-sm-12">Anomaly - Event Registration Payment</h1>
+                        </div>
+                        <div class="row col-sm-12 NoPadding" style="padding-left: 25px;">
+                            <div class="col-lg-12 NoPadding" style="padding-left: 15px;">
+                                <div class="panel NoPadding" style="padding-top: 0px; padding-bottom: 0px; min-height: 50px;">
+                                    <div class="panelheader NoPadding">
+                                        <h2>
+                                            <asp:Label ID="lblPlayerEventCharacter" runat="server" Text="Player - Event - Character"></asp:Label></h2>
+                                    </div>
+                                    <div class="panel-body NoPadding">
+                                        <div class="panel-container NoPadding">
+                                            <asp:Label ID="lblPageText" runat="server">
+                                                Anomaly events are $75.00 if you preregister, $85.00 otherwise. 
+                                                You can pay through PayPal, check, or cash at the door.<br /><br />
+<%--                                                We ask that if you pay in the last few days before an event, please use cash or check at the door,
+                                                since there is a delay before we can access PayPal funds to pay the site fee.<br /><br />
+                                                If you need to make other arrangements, or have questions, please 
+                                                <a href="https://sites.google.com/site/crossoverrules/welcome/contact-info?authuser=0">contact us by email.</a><br /><br />--%>
+                                                <br /><br />
+                                                <a href="https://paypal.me/moshrok@anomalylarp.com/75">Pay through PayPal.</a><br />
+                                            </asp:Label>
+                                            <asp:Label ID="lblEarlyOrLate" runat="server">Anomaly Event (Paid in advance)</asp:Label>
+                                            <div class="paypalWrapper">
+                                                <div class="errors hidden checkoutErrors">
+                                                    <div class="paypalButtonContainer ani_cta_none">
+                                                        <div class="bgradient">
+                                                            <div class="paypalButton">
+                                                                <asp:Label ID="lblPayPalForm" runat="server" Text="lblPayPalForm text here"></asp:Label>
+                                                                <%--<asp:TextBox ID="tbPayPalFormCode" runat="server" Text="" TextMode="MultiLine"></asp:TextBox>--%>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <%--<div class="row PrePostPadding" runat="server">&nbsp;</div>--%>
+                                            <%--<asp:Button ID="btnCalculateOrder" runat="server" CssClass="StandardButton" Text="Calculate Amount" Visible="false" OnClick="btnCalculateOrder_Click" />--%>
+                                            <%--<div class="row PrePostPadding" runat="server">&nbsp;</div>--%>
+                                            <div class="row PrePostPadding" runat="server">
+                                                <div>
+                                                    <asp:HiddenField ID="hidItemName" runat="server" />
+                                                    <%--<asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_s.png" PostBackUrl="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PYM9SG4S63CX2" Visible="true" OnClick="btnPayPalTotal_Click" />--%>
+                                                    <asp:Label ID="lblImageButton" runat="server" Visible="false"></asp:Label><br /><br /><br />
+                                                    <%--Food service payments can be made to Suhayma <a href="https://www.paypal.me/Suhayma" rel="nofollow">here.</a>--%>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row PrePostPadding">&nbsp;</div>
+                        <div class="row PrePostPadding">
+                            <div class="col-sm-11"></div>
+                            <div class="col-sm-1">
+                                <asp:Button ID="Button1" runat="server" CssClass="StandardButton" Text="Close" OnClick="btnClose_Click" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
---
+++ LARPortalUI/Payments/EventPayment7.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Web.UI;
+using LarpPortal.Classes;
+
+namespace LarpPortal.Payments
+{
+    public partial class EventPayment80 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Using session variable RegistrationID look up campaign, event, character
+            int RegistrationID = 0;
+            Session["OnlinePayment"] = "Y";
+            if (Session["RegistrationID"] == null)
+            {
+                Session["RegistrationID"] = 0;  // 8529 Rick Madrigal registration for testing.  Remove this line when being called
+            }
+            int.TryParse(Session["RegistrationID"].ToString(), out RegistrationID);
+            if (RegistrationID == 0)
+            {
+                ClosePage();
+            }
+            else
+            {
+                if (RegistrationID == 0)
+                {
+                    ClientScript.RegisterStartupScript(typeof(Page), "closePage", "window.open('close.html', '_self', null);", true);
+                }
+                else
+                {
+                    string lsRoutineName = "EventPayment.PageLoad";
+                    string stStoredProc = "uspGetPaymentPageCode";
+                    int RoleAlignmentID = 0;
+                    string CharacterAKA = "";
+                    string EventName = "";
+                    string CampaignName = "";
+                    string Player = "";
+                    string strUserName = Session["UserName"].ToString();
+                    int PageCodeID = 0;
+                    int CampaignID = 0;
+                    DateTime EarlyPaymentDate;
+                    bool EarlyPay = false;
+
+                    SortedList slParams = new SortedList();
+                    slParams.Add("@RegistrationID", RegistrationID);
+                    DataTable dtPaymentPageCode = cUtilities.LoadDataTable(stStoredProc, slParams, "LARPortal", strUserName, lsRoutineName);
+                    if (dtPaymentPageCode.Rows.Count == 0)
+                    {
+                        Session["OnlinePayment"] = "N";
+                    }
+                    else
+                    {
+                        foreach (DataRow dRow in dtPaymentPageCode.Rows)
+                        {
+                            int.TryParse(dRow["PaymentPageID"].ToString(), out PageCodeID);
+                            int.TryParse(dRow["RoleAlignmentID"].ToString(), out RoleAlignmentID);
+                            int.TryParse(dRow["CampaignID"].ToString(), out CampaignID);
+                            CharacterAKA = dRow["CharacterAKA"].ToString();
+                            EventName = dRow["EventName"].ToString();
+                            CampaignName = dRow["CampaignName"].ToString();
+                            Player = dRow["Player"].ToString();
+                            DateTime.TryParse(dRow["EarlyPayment"].ToString(), out EarlyPaymentDate);
+                            if (EarlyPaymentDate == null)
+                            {
+                                // Set EarlyPay = true
+                                EarlyPay = true;
+                            }
+                            else
+                            {
+                                if (DateTime.Today > EarlyPaymentDate)
+                                {
+                                    EarlyPay = false;
+                                }
+                                else
+                                {
+                                    EarlyPay = true;
+                                }
+                            }
+
+                            switch (RoleAlignmentID)
+                            {
+                                case 2:
+                                    CharacterAKA = "NPC";
+                                    break;
+                                case 3:
+                                    CharacterAKA = "Staff";
+                                    break;
+                                default:
+                                    CharacterAKA = "Character: " + CharacterAKA;
+                                    break;
+                            }
+                            hidItemName.Value = EventName + " - " + CharacterAKA;
+                            lblPlayerEventCharacter.Text = "Player: " + Player + " - " + hidItemName.Value;
+
+                            lblPayPalForm.Text = "<form class=\"paypalForm\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_blank\" novalidate=\"novalidate\">";
+                            lblPayPalForm.Text += "<input name=\"business\" type=\"hidden\" value=\"sebastian.sarr@gmail.com\" />";
+                            lblPayPalForm.Text += "<input name=\"item_name\" type=\"hidden\" value=\"" + lblPlayerEventCharacter.Text + "\" />";
+                            lblPayPalForm.Text += "<input name=\"bn\" type=\"hidden\" value=\"POWr_SP\" />";
+                            lblPayPalForm.Text += "<input name=\"charset\" type=\"hidden\" value=\"UTF-8\" />";
+                            lblPayPalForm.Text += "<input name=\"on0\" type=\"hidden\" value=\"Event\" />";
+                            lblPayPalForm.Text += "<input name=\"currency_code\" type=\"hidden\" value=\"USD\" />";
+                            lblPayPalForm.Text += "<input name=\"cmd\" type=\"hidden\" value=\"_xclick\" />";
+                            if (EarlyPay == true)
+                            {
+                                lblPayPalForm.Text += "<input name=\"amount\" type=\"hidden\" value=\"80.00\" />";
+                                lblEarlyOrLate.Text = "Crossover Event (Paid in advance)";
+                            }
+                            else
+                            {
+                                lblPayPalForm.Text += "<input name=\"amount\" type=\"hidden\" value=\"85.00\" />";
+                                lblEarlyOrLate.Text = "Crossover Event (Past 3 Week Deadline)";
+                            }
+                            lblPayPalForm.Text += "<input name=\"no_shipping\" type=\"hidden\" value=\"1\" />";
+                            //lblPayPalForm.Text += "<input name=\"undefined_quantity\" type=\"hidden\" value=\"1\" />";
+                            //lblPayPalForm.Text += "<input name=\"quantity\" type=\"hidden\" value=\"1\" />";
+                            //lblPayPalForm.Text += "<input name=\"return\" type=\"hidden\" value=\"https://www.powr.io/plugins/\" />";
+                            //lblPayPalForm.Text += "<input name=\"rm\" type=\"hidden\" value=\"1\" />";
+                            //lblPayPalForm.Text += "<input name=\"notify_url\" type=\"hidden\" value=\"https://www.powr.io/payment_notification/5262016\" />";
+                            //lblPayPalForm.Text += "<div class=\"submitButton fitText\" style=\"font-size: 14px;\">Pay With PayPal</div>";
+                            lblPayPalForm.Text += "<div class=\"submitButton fitText\" style=\"font-size: 14px;\"></div>";
+                            lblPayPalForm.Text += "</form>";
+                            //tbPayPalFormCode.Text = lblPayPalForm.Text;
+                            lblImageButton.Text = "<input type=\"image\" name=\"ImageButton1\" id=\"ImageButton1\"";
+                            lblImageButton.Text += " src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_s.png\"";
+                            lblImageButton.Text += " onclick=\"javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ImageButton1&quot;,";
+                            //lblImageButton.Text += "  &quot;&quot;, false, &quot;&quot;, &quot;https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=PYM9SG4S63CX2&quot;, ";
+                            lblImageButton.Text += "  &quot;&quot;, false, &quot;&quot;, &quot;https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp&quot;, ";
+                            lblImageButton.Text += "  false, false))\" /><span id=\"lblImageButton\"></span>";
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void btnPayPalTotal_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            ClosePage();
+        }
+
+        protected void ClosePage()
+        {
+            ClientScript.RegisterStartupScript(typeof(Page), "closePage", "window.close();", true);
+        }
+
+    }
+}
